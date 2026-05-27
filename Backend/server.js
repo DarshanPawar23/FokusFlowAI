1ce39f2577609948184590ac52221e1f41ec5d30
DarshanPawar23 <darshansp0230@gmail.com>
2026-05-27 23:46:51 +0530

    cors_updated

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,12 @@ dotenv.config();
 const app = express();
 let port = 3000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://fokus-flow-ai.vercel.app/",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/api/users", userRoutes);
